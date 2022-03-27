458d9c019535d52f38e83da9c574dc9c777ae27c
skillor <skillor@gmx.net>
2022-03-27 01:32:36 +0100

    more typing

--- imagestack_svg/imagestack.py
+++ imagestack_svg/imagestack.py
@@ -13,10 +13,12 @@ class ImageStack:
         return self.svg
 
     @staticmethod
-    async def _create_bytes(svg: str, max_size: tuple = None):
+    async def _create_bytes(svg: str,
+                            image_format: QImage.Format = QImage.Format_RGBA64,
+                            max_size: tuple = None) -> io.BytesIO:
         svg = f'{SVG_PREFIX}{svg}{SVG_SUFFIX}'
         svg_renderer = QSvgRenderer(svg.encode())
-        orig_svg = QImage(svg_renderer.defaultSize(), QImage.Format_RGBA64)
+        orig_svg = QImage(svg_renderer.defaultSize(), image_format)
         painter = QPainter(orig_svg)
 
         svg_renderer.render(painter)
@@ -30,17 +32,20 @@ class ImageStack:
         buffer.seek(0)
         return io.BytesIO(buffer.readAll())
 
-    async def create_bytes(self, image_creator: ImageCreator, max_size: tuple = None):
-        return self._create_bytes(self.svg, max_size)
+    async def create_bytes(self,
+                           image_creator: ImageCreator,
+                           image_format: QImage.Format = QImage.Format_RGBA64,
+                           max_size: tuple = None):
+        return self._create_bytes(self.svg, image_format, max_size)
 
-    async def _create_raw_svg(self, svg: str):
+    async def _create_raw_svg(self, svg: str) -> str:
         return f'{SVG_PREFIX}{svg}{SVG_SUFFIX}'
 
-    async def create_raw_svg(self, image_creator: ImageCreator):
+    async def create_raw_svg(self, image_creator: ImageCreator) -> str:
         return await self._create_raw_svg(self.svg)
 
-    async def _create_inner_svg(self, svg: str):
+    async def _create_inner_svg(self, svg: str) -> str:
         return svg
 
-    async def create_inner_svg(self, image_creator: ImageCreator):
+    async def create_inner_svg(self, image_creator: ImageCreator) -> str:
         return await self._create_inner_svg(self.svg)
